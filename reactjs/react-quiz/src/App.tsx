bdec8395b1de8980788c93902e42af832b7b75df
isuru <isuru.b@intervest.lk>
2024-05-28 18:54:43 +0530

    Update code

--- reactjs/react-quiz/src/App.tsx
+++ reactjs/react-quiz/src/App.tsx
@@ -1,8 +1,52 @@
-import { Heading } from "@chakra-ui/react";
+import { Box, Flex, Image } from "@chakra-ui/react";
+import logoImg from "./assets/logo.png";
+import bubbleImg from "./assets/bubble.png";
 import "./App.css";
+import { useState } from "react";
+import { SetQuestionQty } from "./features/SetQuestionQty";
 
-function App() {
-  return <Heading>sjsd</Heading>;
+enum Step {
+  SetQuestionQty,
+  SetQuestionCategory,
+  SetQuestionDifficulty,
+  Play,
+  Score,
 }
+export default function App() {
+  const [step, setStep] = useState<Step>(Step.SetQuestionQty);
+  const header = (
+    <Flex justify="center">
+      <Image h="24" src={logoImg} />
+    </Flex>
+  );
 
-export default App;
+  const renderScreenByStep = () => {
+    switch (step) {
+      case Step.SetQuestionQty:
+        return <SetQuestionQty defaultValue={10} max={30} min={5} step={5} />;
+      case Step.SetQuestionCategory:
+        return <></>;
+      case Step.SetQuestionDifficulty:
+        return <></>;
+      case Step.Play:
+        return <></>;
+      case Step.Score:
+        return <></>;
+      default:
+        return null;
+    }
+  };
+  return (
+    <Box py={"10"} h="100%">
+      {header}
+      <Image
+        src={bubbleImg}
+        position={"absolute"}
+        zIndex={-1}
+        right={-120}
+        top={100}
+      />
+      <Box mt={100}>{renderScreenByStep()}</Box>
+    </Box>
+  );
+}
